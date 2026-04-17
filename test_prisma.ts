bb51566daa131a5bb61eebfb87b18560d759cc5e
Tuan Le <tuanle200577@gmail.com>
2026-04-17 11:47:20 -0500

    fix: removed test_prisma

--- test_prisma.ts
+++
@@ -1,7 +0,0 @@
-import { PrismaClient } from '@prisma/client'
-try {
-  const prisma = new PrismaClient({ log: ['info'] })
-  console.log("Success with log options")
-} catch (e) {
-  console.log(e.message)
-}
